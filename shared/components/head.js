753c1b35d7385a4e89a740805a0a626476825a63
RafaelDoering <rafael.doering.soares@usp.br>
2019-07-21 22:04:25 -0300

    favicon

--- shared/components/head.js
+++ shared/components/head.js
@@ -1,23 +1,47 @@
 import React from 'react';
 import HeadNext from 'next/head';
-import { string } from 'prop-types';
 
 const Head = props => (
   <HeadNext>
-    <meta charSet="UTF-8" />
+    <meta charSet='UTF-8' />
     <title>data ICMC</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="stylesheet" href="/static/styles/tailwind.min.css" />
-    <link rel="stylesheet" href="/static/styles/style.css" />
-    <link href="/static/webfonts/roboto/stylesheet.css" rel="stylesheet" />
+    <link
+      rel='apple-touch-icon'
+      sizes='180x180'
+      href='/static/favicon/apple-touch-icon.png'
+    />
+    <link
+      rel='icon'
+      type='image/png'
+      sizes='32x32'
+      href='/static/favicon/favicon-32x32.png'
+    />
+    <link
+      rel='icon'
+      type='image/png'
+      sizes='16x16'
+      href='/static/favicon/favicon-16x16.png'
+    />
+    <link rel='manifest' href='/static/favicon/manifest.json' />
+    <link
+      rel='mask-icon'
+      href='/static/favicon/safari-pinned-tab.svg'
+      color='#7fd3f5'
+    />
+    <link rel='shortcut icon' href='/static/favicon/favicon.ico' />
+    <meta name='apple-mobile-web-app-title' content='data ICMC' />
+    <meta name='application-name' content='data ICMC' />
+    <meta name='msapplication-TileColor' content='#ffffff' />
+    <meta
+      name='msapplication-config'
+      content='/static/favicon/browserconfig.xml'
+    />
+    <meta name='theme-color' content='#ffffff' />
+    <meta name='viewport' content='width=device-width, initial-scale=1' />
+    <link rel='stylesheet' href='/static/styles/tailwind.min.css' />
+    <link rel='stylesheet' href='/static/styles/style.css' />
+    <link href='/static/webfonts/roboto/stylesheet.css' rel='stylesheet' />
   </HeadNext>
 );
-
-Head.propTypes = {
-  title: string,
-  description: string,
-  url: string,
-  ogImage: string
-};
 
 export default Head;
