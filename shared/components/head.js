f3354c8a47aae21f032d95278904cd02ca4e0293
RafaelDoering <rafael.doering.soares@usp.br>
2019-07-21 21:47:59 -0300

    page title

--- shared/components/head.js
+++ shared/components/head.js
@@ -2,22 +2,11 @@ import React from 'react';
 import HeadNext from 'next/head';
 import { string } from 'prop-types';
 
-const defaultTitle = '';
-const defaultDescription = '';
-const defaultOGURL = '';
-const defaultOGImage = '';
-
 const Head = props => (
   <HeadNext>
     <meta charSet="UTF-8" />
-    <title>{props.title || defaultTitle}</title>
+    <title>data ICMC</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="twitter:site" content={props.url || defaultOGURL} />
-    <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:image" content={props.ogImage || defaultOGImage} />
-    <meta property="og:image" content={props.ogImage || defaultOGImage} />
-    <meta property="og:image:width" content="1200" />
-    <meta property="og:image:height" content="630" />
     <link rel="stylesheet" href="/static/styles/tailwind.min.css" />
     <link rel="stylesheet" href="/static/styles/style.css" />
     <link href="/static/webfonts/roboto/stylesheet.css" rel="stylesheet" />
